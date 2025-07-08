da9bd9cfbb4d7f3b10cd4dd4ee708eb76ce1ad35
Omkar-doc <omkarraut654@gmail.com>
2025-07-08 13:43:12 +0530

    new changes in s3 nd db

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraformproject02"
+    bucket = "projectproject07072025"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
