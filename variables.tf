ff98aa8295a0ff56ca60355258b9912cea600feb
Omkar-doc <omkarraut654@gmail.com>
2025-07-08 13:59:58 +0530

    new changes in s3 nd db add key

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "new-project" 
+    default     =  "Project-key (1)" 
 }
 
 variable "instance_type" { 
